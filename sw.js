177d3f62bb624525d26ddd783f548c00f34f4aaa
Matheuscosta-mc <costa6503@gmail.com>
2026-05-13 22:13:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'financas-sb-v3';
+const CACHE = 'financas-sb-v4';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
